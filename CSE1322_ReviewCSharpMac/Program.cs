bf8c15dfe7b5fa31932297293de1b9dd953ec88b
CSITEd Experts <CSITEdExperts@gmail.com>
2020-04-12 20:51:15 -0400

    CSE1322_ReviewCSharpMac added

---
+++ CSE1322_ReviewCSharpMac/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSE1322_ReviewCSharpMac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
